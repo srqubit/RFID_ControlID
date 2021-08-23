14b9d0d484595a7b60a0a225f80d22a11f4bbd1c
srqubit <gustavo@msiautomacao.com.br>
2021-08-23 17:03:48 -0300

    Criaçao sistema de proteçao

--- inicializar.py
+++ inicializar.py
@@ -21,28 +21,33 @@ while True:
         if (vSegundos != vAgora.strftime('%S')):
             #atualizada data e hora
             vSegundos = vAgora.strftime('%S')
-
+            print (vSegundos)
+            
             #Aguarda RFID   
             VCartao = read_rfid(porta=vPorta, velocidade=vVelocidade)
-            print(VCartao)
-            #--------------------------------------------------------------------------------
-            #Gravar arquivo de saída
-            with open(vArquivoSaida, 'r') as f:
-                vSaidasCB = json.loads(str(f.read()).strip("'<>() ").replace('\'', '\"')) 
+            
+            #VCartao = read_rfid(porta=vPorta, velocidade=vVelocidade)
+            
+            if VCartao!='':
+                print(VCartao)
+                #--------------------------------------------------------------------------------
+                #Gravar arquivo de saída
+                with open(vArquivoSaida, 'r') as f:
+                    vSaidasCB = json.loads(str(f.read()).strip("'<>() ").replace('\'', '\"')) 
 
-            vSaidasCB["rfid"]["ultimocartao"] = VCartao
+                vSaidasCB["rfid"]["ultimocartao"] = VCartao
 
 
-            #Trata passagem de cartão repetidas vezes
-            vDiffSeg = datetime.now()-vUltaDataHora
-            vUltaDataHora = datetime.now()
+                #Trata passagem de cartão repetidas vezes
+                vDiffSeg = datetime.now()-vUltaDataHora
+                vUltaDataHora = datetime.now()
 
-            print (vDiffSeg.seconds)
-            #Verifica se o de agora for diferente do ativo e tiver mais de 2 segundos ativa cartão
-            if VCartao != vSaidasCB["rfid"]["cartaoativo"] and VCartao!=0 and vDiffSeg.seconds > 2:
-                vSaidasCB["rfid"]["cartaoativo"] = VCartao
+                print (vDiffSeg.seconds)
+                #Verifica se o de agora for diferente do ativo e tiver mais de 2 segundos ativa cartão
+                if VCartao != vSaidasCB["rfid"]["cartaoativo"] and VCartao!=0 and vDiffSeg.seconds > 2:
+                    vSaidasCB["rfid"]["cartaoativo"] = VCartao
 
-            with open(vArquivoSaida, 'w') as json_file:
-                json.dump(vSaidasCB, json_file)
+                with open(vArquivoSaida, 'w') as json_file:
+                    json.dump(vSaidasCB, json_file)
     except:
         print("erro RFID " + vAgora.strftime('%H:%M:%S'))
--- rfid.py
+++ rfid.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python
 # coding=utf-8
-
+from threading import Timer
+import argparse
+import binascii
 import serial
 
 def read_rfid(porta, velocidade):
     try:
         ser = serial.Serial(porta)
         ser.baudrate = int(velocidade)
-        daten = str(ser.read(14))
+        ser.timeout=10
+        ser.flushInput()
         
-        ser.close()    
-        daten = daten.replace("\\x02", "" )
-        daten = daten.replace("\\x03", "" )
-        daten = daten.replace("\\r\\n", "" )
-        daten = daten.replace("b'", "" )
+        daten = ser.readline().strip()
+        ser.close()        
+        
+        hexo = str(daten)
+        
+        if str(hexo)=="b''":
+            return ""
+        
+        daten = '{:0>8}'.format(hexo.upper())
+        daten = daten.replace("B'\\X02", "" )
         daten = daten.replace("'", "" )
-        daten = daten.replace(" ", "" )
         daten = str(hex(int(daten)))
         daten = daten.replace("0x", "" )
-
-        return daten.upper()
-    except:
-        return 0
+        
+        return "3C" + '{:0>8}'.format(daten.upper())
+    except Exception as e:
+        print("Erro leitura RFID " + str(e))
+        return ""
